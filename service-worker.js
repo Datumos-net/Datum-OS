62e7889efccee5e313ac0465a0f4d5a8f4baca7c
Datum-OS-bit <mail.datumos@gmail.com>
2026-06-19 22:23:34 +0530

    Add files via upload
    Initial commit for Datum OS

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'datum-os-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+// Install and cache the files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Serve from cache if offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
